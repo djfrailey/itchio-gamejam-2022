4ba3e18db6e677ef2228dfc89bdd1c66b94259f4
David Frailey <d.j.frailey@gmail.com>
2022-11-12 16:15:16 -0800

    Changed idle detection

--- scripts/PlayerController.gd
+++ scripts/PlayerController.gd
@@ -11,17 +11,31 @@ var _can_move : bool = true # Used to prevent Player from moving while Punching.
 
 ## OVERRIDES
 
+func _ready():
+	_animPlayer.connect("animation_finished", self, "_on_animPlayer_animation_finished")
+
 func _process(delta):
 	if (_can_move):
 		_handle_movement(delta)
+		
+	_handle_punch()
 	
-	if (_is_idle()):
+	if (_animPlayer.current_animation == "" or _animPlayer.current_animation == _IDLE_ANIMATION):
+		_can_move = true
 		_start_animation(_IDLE_ANIMATION)
-		
-		
+
 ## PUBLIC METHODS
 
 ## PRIVATE METHODS
+
+func _on_animPlayer_animation_finished(anim_name : String):
+	if (anim_name == _PUNCH_ANIMATION):
+		_start_animation(_IDLE_ANIMATION)
+
+func _handle_punch():
+	if (Input.is_action_just_pressed("ui_select")):
+		_start_animation(_PUNCH_ANIMATION)
+		_can_move = false
 
 func _handle_movement(delta):
 	if (Input.is_action_pressed("ui_right")):
@@ -30,12 +44,8 @@ func _handle_movement(delta):
 	elif (Input.is_action_pressed("ui_left")):
 		_sprite.flip_h = true
 		_start_animation(_WALK_ANIMATION)
-		
-func _is_idle():
-	return Input.is_action_pressed("ui_right") == false and Input.is_action_pressed("ui_left") == false and Input.is_action_pressed("ui_select") == false
 
 func _start_animation(animation : String):
 	assert(_animPlayer.has_animation(animation) == true, "Requested animation not present in PlayerController Requested:" + animation)
-	
 	if (_animPlayer.current_animation != animation):
 		_animPlayer.play(animation)
